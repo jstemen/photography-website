c75e822d3f8380a83414df43b1e07d1d3869b867
Jared Stemen <jstemen@gmail.com>
2013-12-01 20:37:23 -0500

    bootstrap is working

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+ruby "1.9.3"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PalaksPhotoWebsite::Application.routes.draw do
   resources :photos
+  root 'photos#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
